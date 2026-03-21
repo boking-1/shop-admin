43faa9d11a2d52b99f6b13f2de3e06d4d6da7e62
202321013402 <2493392002@qq.com>
2026-03-21 20:00:38 +0800

    feat: 图库组件开发-添加图像分类功能，整合 API 调用并更新侧边栏组件以显示分类列表

--- src/components/imageAside.vue
+++ src/components/imageAside.vue
@@ -1,19 +1,18 @@
 <template>
 
-    <el-aside width="220px" class="image-aside">
+    <el-aside width="220px" class="image-aside" v-loading="loading">
         <div class="top">
-            <aside-list active>
-                分类标题
-            </aside-list>   
-            <aside-list>
-                分类标题
+            <aside-list v-for="(item, index) in list" ::key="index" :active="activeId == item.id">
+                {{ item.name }}
             </aside-list>
+
         </div>
 
         <div class="bottom">
-            分页区域
+            <el-pagination background layout="prev, next" :total="total" :current-page="currentPage" :page-size="limit"
+                @current-change="getData" />
         </div>
-    </el-aside>             
+    </el-aside>
 </template>
 
 <style>
@@ -41,6 +40,35 @@
 }
 </style>
 <script setup>
+import { getImageClassList } from '~/api/image_class.js';
+import { ref } from 'vue'
 import AsideList from './AsideList.vue';
+
+const loading = ref(false)
+const list = ref([])
+const currentPage = ref(1)
+const total = ref(0)
+const activeId = ref(0)
+const limit = ref(10)
+
+function getData(p = null) {
+
+    if (typeof p == "number") {
+        currentPage.value = p
+    }
+
+    loading.value = true
+    getImageClassList(currentPage.value)
+        .then(res => {
+            total.value = res.totalCount
+            list.value = res.list
+            if (list.value[0]) {
+                activeId.value = list.value[0].id
+            }
+
+        })
+        .finally(() => loading.value = false)
+}
+getData()
 
 </script>
