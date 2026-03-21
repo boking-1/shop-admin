77793df48f2856671b631820fbadf68df0cbe5a1
202321013402 <2493392002@qq.com>
2026-03-21 14:17:15 +0800

    feat: 图库模块开发-布局实现，添加 imageAside 和 imageMain 组件，并在图库列表页面中整合使用

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,35 @@
 </template>
 
 <style>
-body{
+body {
     @apply bg-gray-200;
 }
+
 #nprogress .bar {
     background: #d1d5db !important;
     height: 3px !important;
+}
+
+::-webkit-scrollbar {
+    width: 4px;
+    height: 6px;
+
+}
+
+::-webkit-scrollbar-corner {
+    display: block;
+}
+
+::-webkit-scrollbar-thumb {
+    border-radius: 8px;
+    background-color: rgba(0, 0, 0, 0.2);
+}
+
+::-webkit-scrollbar-thumb,
+::-webkit-scrollbar-track {
+    border-right-color: transparent;
+    border-left-color: transparent;
+    background-color: rgba(0, 0, 0, 0.2);
+
 }
 </style>
--- src/pages/image/list.vue
+++ src/pages/image/list.vue
@@ -1,5 +1,26 @@
 <template>
     <div>
-        图库列表
+        <el-container class="bg-white rounded" :style="{ height: (h + 'px') }">
+            <el-header class="image-header">Header</el-header>
+            <el-container>
+                <imageAside/>
+                <imageMain/>
+            </el-container>
+        </el-container>
     </div>
 </template>
+<style>
+.image-header {
+    border-bottom: 1px solid #eeeeee;
+    @apply flex items-center;
+}
+
+
+</style>
+
+<script setup>
+import imageAside from '~/components/imageAside.vue';
+import imageMain from '~/components/imageMain.vue';
+const windowHeight = window.innerHeight || document.body.clientHeight
+const h = windowHeight - 64 - 44 - 40
+</script>
