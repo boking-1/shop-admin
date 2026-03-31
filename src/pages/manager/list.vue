07d814203d9f3c68ab46c66a412015cceb0c871f
202321013402 <2493392002@qq.com>
2026-03-31 16:21:26 +0800

    feat: 添加管理员管理功能，实现新增，修改，删除管理员

--- src/pages/manager/list.vue
+++ src/pages/manager/list.vue
@@ -47,7 +47,7 @@
                     </template>
                 </el-table-column>
 
-                <el-table-column label="所属管理员">
+                <el-table-column label="所属角色">
                     <template #default="{ row }">
                         {{ row.role?.name || "-" }}
                     </template>
@@ -88,11 +88,25 @@
         <FormDrawer :title="drawerTitle" ref="formDrawerRef" @submit="handleSubmit">
 
             <el-form :model="form" ref="formRef" :rules="rules" label-width="80px" :inline="false" size="default">
-                <el-form-item label="公告标题" prop="title">
-                    <el-input v-model="form.title" placeholder="公告标题"></el-input>
+                <el-form-item label="用户名" prop="username">
+                    <el-input v-model="form.username" placeholder="用户名"></el-input>
                 </el-form-item>
-                <el-form-item label="公告内容" prop="content">
-                    <el-input v-model="form.content" placeholder="公告内容" type="textarea" :rows="5"></el-input>
+                <el-form-item label="密码" prop="password">
+                    <el-input v-model="form.password" placeholder="密码"></el-input>
+                </el-form-item>
+                <el-form-item label="头像" prop="avatar">
+                    <el-input v-model="form.avatar"></el-input>
+                </el-form-item>
+                <el-form-item label="所属角色" prop="content">
+                    <el-select v-model="form.role_id" placeholder="选择所属角色">
+                        <el-option v-for="item in roles" :key="item.id" :label="item.name" :value="item.id">
+                        </el-option>
+                    </el-select>
+
+                </el-form-item>
+                <el-form-item label="状态" prop="content">
+                    <el-switch v-model="form.status" :active-value="1" :inactive-value="0">
+                    </el-switch>
                 </el-form-item>
             </el-form>
         </FormDrawer>
@@ -102,8 +116,7 @@
 <script setup>
 import { toast } from '~/composables/util'
 import { ref, reactive, computed } from 'vue'
-import { getNoticeList, createNotice, updateNotice, deleteNotice } from '~/api/notice'
-import { getManagerList, updateManagerStatus } from '~/api/manager'
+import { getManagerList, updateManagerStatus, createManager, updateManager, deleteManager } from '~/api/manager'
 import FormDrawer from '~/components/FormDrawer.vue'
 //搜索
 const searchForm = reactive({
@@ -115,6 +128,7 @@ const resetSearchForm = () => {
 }
 //公告列表数据
 const tableData = ref([])
+const roles = ref([])
 //加载动画
 const loading = ref(false)
 //分页
@@ -129,29 +143,19 @@ const formDrawerRef = ref(null)
 //表单
 const formRef = ref(null)
 const form = reactive({
-    title: "",
-    content: ""
+    username: "",
+    password: "",
+    role_id: "null",
+    status: 1,
+    avatar: ""
 })
 //表单验证规则
 const rules = {
-    title: [
-        {
-            required: true,
-            message: '公告标题不能为空',
-            trigger: 'blur'
-        },
-    ],
-    content: [
-        {
-            required: true,
-            message: '公告内容不能为空',
-            trigger: 'blur'
-        },
-    ],
+
 }
 
-const editId = ref(0)//若为0，抽屉即为新增公告功能，若为公告id，即为修改功能
-const drawerTitle = computed(() => editId.value ? '修改公告' : '新增公告')
+const editId = ref(0)//若为0，抽屉即为新增管理员功能，若为管理员id，即为修改功能
+const drawerTitle = computed(() => editId.value ? '修改管理员' : '新增管理员')
 //重置表单
 function resetForm(row = null) {
     if (formRef.value) {
@@ -162,29 +166,32 @@ function resetForm(row = null) {
             form[key] = row[key]
     }
 }
-//新增公告
+//新增管理员
 const handleCreate = () => {
     resetForm({
-        title: '',
-        content: ''
+        username: "",
+        password: "",
+        role_id: null,
+        status: 1,
+        avatar: ""
     })
     editId.value = 0
     formDrawerRef.value.open()
 }
-//修改公告
+//修改管理员
 const handleUpdate = (row) => {
     editId.value = row.id
     resetForm(row)
     formDrawerRef.value.open()
 }
-//提交表单-新增或修改公告
+//提交表单-新增或修改管理员
 const handleSubmit = () => {
     formRef.value.validate(valid => {
         if (!valid) return
         formDrawerRef.value.showLoading()
         console.log(editId.value);
 
-        const fun = editId.value ? updateNotice(editId.value, form) : createNotice(form)
+        const fun = editId.value ? updateManager(editId.value, form) : createManager(form)
         fun.then(res => {
             toast(editId.value ? "修改成功" : "新增成功")
             //修改刷新当前页，新增刷新第一页
@@ -196,10 +203,10 @@ const handleSubmit = () => {
             })
     })
 }
-//删除公告
+//删除管理员
 const handleDelete = (id) => {
     loading.value = true
-    deleteNotice(id)
+    deleteManager(id)
         .then(res => {
             toast("删除成功")
             getData()
@@ -215,6 +222,7 @@ const getData = (p = null) => {
     loading.value = true
     getManagerList(currentPage.value, searchForm)
         .then((res) => {
+            roles.value = res.roles
             tableData.value = res.list.map(o => {
                 o.statusLoading = false
                 return o
