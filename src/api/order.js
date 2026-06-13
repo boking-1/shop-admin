1c3786e156e7f0c8098b0d213e5301efd6f885fd
202321013402 <2493392002@qq.com>
2026-06-13 18:39:30 +0800

    feat: 新增订单管理页面，包含订单列表、搜索、分页和批量操作功能

---
+++ src/api/order.js
@@ -0,0 +1,14 @@
+import axios from '~/axios'
+import { queryParams } from '~/composables/util.js'
+
+export function getOrderList(page, query = {}) {
+    let r = queryParams(query)
+    return axios.get(`admin/order/${page}${r}`,query)
+}
+
+export function deleteOrder(ids) {
+    ids = !Array.isArray(ids) ? [ids] : ids
+
+    return axios.post(`admin/order/delete_all`, { ids })
+}
+
--- src/pages/order/list.vue
+++ src/pages/order/list.vue
@@ -1,5 +1,243 @@
 <template>
     <div>
-        订单列表
+        <div>
+            <el-tabs v-model="searchForm.tab" class="demo-tabs" @tab-change="getData">
+                <el-tab-pane :label="item.name" :name="item.key" v-for="(item, index) in tabbars"
+                    :key="index"></el-tab-pane>
+            </el-tabs>
+        </div>
+        <el-card shadow="never" :body-style="{ padding: '20px' }">
+            <!-- 搜索 -->
+            <Search :model="searchForm" @reset="resetSearchForm" @search="getData">
+                <SearchItem label="订单编号">
+                    <el-input v-model="searchForm.no" placeholder="订单编号" clearable></el-input>
+                </SearchItem>
+
+                <template #show>
+                    <SearchItem label="收货人">
+                        <el-input v-model="searchForm.name" placeholder="收货人" clearable></el-input>
+                    </SearchItem>
+                    <SearchItem label="手机号">
+                        <el-input v-model="searchForm.phone" placeholder="手机号" clearable></el-input>
+                    </SearchItem>
+                    <SearchItem label="开始时间">
+                        <el-date-picker v-model="searchForm.starttime" type="date" placeholder="开始日期"
+                            style="width: 90%;" value-format="YYYY-MM-DD">
+                        </el-date-picker>
+
+                    </SearchItem>
+                    <SearchItem label="结束时间">
+                        <el-date-picker v-model="searchForm.endtime" type="date" placeholder="结束日期" style="width: 90%;"
+                            value-format="YYYY-MM-DD">
+                        </el-date-picker>
+                    </SearchItem>
+                </template>
+
+            </Search>
+
+
+            <!-- 新增|刷新 -->
+            <ListHeader layout="refresh" @refresh="getData">
+
+                <el-button class="ml-2" size="small" @click="multiSelectionDelete" type="danger">
+                    批量删除
+                </el-button>
+            </ListHeader>
+
+            <el-table ref="multipleTableRef" :data="tableData" stripe style="width: 100%" v-loading="loading"
+                @selection-change="handleSelectionChange">
+                <el-table-column type="selection" width="55" />
+
+                <!-- 商品 -->
+                <el-table-column label="商品">
+                    <template #default="{ row }">
+                        <div class="flex text-sm">
+                            <div class="flex-1">
+                                <p>订单号</p>
+                                <small>{{ row.no }}</small>
+                            </div>
+                            <div>
+                                <p>下单时间</p>
+                                <small>{{ row.create_time }}</small>
+                            </div>
+                        </div>
+                        <div class="flex" v-for="(item, index) in row.order_items" :key="index">
+                            <el-image :src="item.goods_item ? item.goods_item.cover : ''" fit="cover" :lazy="true"
+                                style="width: 30px;height: 30px;"></el-image>
+                            <p class=" text-blue-500 ml-2">
+                                {{ item.goods_item ? item.goods_item.title : '商品已被删除' }}
+                            </p>
+                        </div>
+                    </template>
+                </el-table-column>
+                <!-- 实际付款 -->
+                <el-table-column label="实际付款" width="120" prop="total_price" align="center" />
+                <!-- 买家 -->
+                <el-table-column label="买家" width="120" align="center">
+                    <template #default="{ row }">
+                        <p>{{ row.user.nickname || row.user.username }}</p>
+                        <small>(用户ID{{ row.user.id }})</small>
+                    </template>
+                </el-table-column>
+                <!-- 交易状态 -->
+                <el-table-column label="审核状态" width="120" align="center">
+                    <template #default="{ row }">
+                        <div>
+                            付款状态:
+                            <el-tag type="primary" size="small" v-if="row.payment_method == 'alipay'">支付宝支付</el-tag>
+                            <el-tag type="success" size="small" v-else-if="row.payment_method == 'wechat'">微信支付</el-tag>
+                            <el-tag type="info" size="small" v-else>未支付</el-tag>
+                        </div>
+                        <div>
+                            发货状态:
+                            <el-tag :type="row.ship_data ? 'success' : 'info'" size="small">{{ row.ship_data ? '已发货' :
+                                '未发货' }}</el-tag>
+                        </div>
+                        <div>
+                            收货状态:
+                            <el-tag :type="row.ship_status == 'received' ? 'success' : 'info'" size="small">{{
+                                row.ship_status == 'received' ? '已收货' : '未收货' }}</el-tag>
+                        </div>
+                    </template>
+                </el-table-column>
+
+                <!-- 操作 -->
+                <el-table-column label="操作" align="center">
+                    <template #default="{ row }">
+                        <div v-if="searchForm.tab != 'delete'">
+                            <el-button class="px-1" type="primary" size="small" text @click="">订单详情</el-button>
+                            <el-button v-if="searchForm.tab == 'noship'" class="px-1" type="primary" size="small" text
+                                @click="">订单发货</el-button>
+                            <el-button v-if="searchForm.tab == 'refunding'" class="px-1" type="primary" size="small"
+                                text @click="">同意退款</el-button>
+                            <el-button v-if="searchForm.tab == 'refunding'" class="px-1" type="primary" size="small"
+                                text @click="">拒绝退款</el-button>
+
+
+                        </div>
+                        <span v-else>暂无操作</span>
+                    </template>
+                </el-table-column>
+            </el-table>
+
+        </el-card>
+        <!-- 分页 -->
+        <div class=" flex items-center justify-center mt-3 ">
+            <el-pagination background layout="prev, pager,next" :total="total" :current-page="currentPage"
+                :page-size="limit" @current-change="getData" />
+        </div>
     </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { getOrderList, deleteOrder } from '~/api/order'
+import Search from '~/components/Search.vue'
+import SearchItem from '~/components/SearchItem.vue'
+import { useInitTable } from '~/composables/useCommon.js'
+import ListHeader from '~/components/ListHeader.vue'
+import { getCategoryList } from '~/api/category'
+import { toast } from '~/composables/util.js'
+
+//列表，分页，搜索
+const {
+    searchForm,
+    resetSearchForm,
+    tableData,
+    loading,
+    currentPage,
+    total,
+    limit,
+    getData,
+    handleDelete,
+    handleMultiStatusChange,
+    handleSelectionChange,
+    multiSelectionDelete,
+    multipleTableRef,
+    multiSelectionIds
+} = useInitTable({
+    searchForm: {
+        tab: "all",
+        no: "",
+        starttime: "",
+        endtime: "",
+        name: "",
+        phone: ""
+    },
+    getList: getOrderList,
+    delete: deleteOrder,
+    onGetListSuccess: (res) => {
+        tableData.value = res.list.map(o => {
+            o.statusLoading = false
+            o.bannersLoading - false
+            o.contentLoading = false
+            o.skusLoading = false
+            return o
+        })
+        total.value = res.totalCount
+
+    }
+})
+
+// 商品标签页
+const tabbars = [
+    {
+        key: "all",
+        name: "全部"
+    },
+    {
+        key: "nopay",
+        name: "待支付"
+    },
+    {
+        key: "noship",
+        name: "代发货"
+    },
+    {
+        key: "noreceived",
+        name: "代收货"
+    },
+    {
+        key: "finish",
+        name: "已完成"
+    },
+    {
+        key: "closed",
+        name: "已关闭"
+    },
+    {
+        key: "refunding",
+        name: "退款中"
+    },
+]
+
+//商品分类列表
+const categoryList = ref([])
+getCategoryList().then((res) => {
+    categoryList.value = res
+})
+
+//彻底删除
+const handleDestoryGoods = () => {
+    useMultiAction(destoryGoods, "彻底删除")
+}
+
+// 批量操作:恢复或彻底删除
+function useMultiAction(func, msg) {
+    loading.value = true
+    func(multiSelectionIds.value)
+        .then(res => {
+            toast(msg + "成功")
+            //清空选中
+            if (multipleTableRef.value) {
+                multipleTableRef.value.clearSelection()
+            }
+            getData()
+        })
+        .finally(() => {
+            loading.value = false
+        })
+}
+
+
+</script>
